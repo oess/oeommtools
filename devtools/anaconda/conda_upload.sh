c3f874e93d66fb136db8d1c87d63e4b8aefd67c7
nividic <gcalabro@uci.edu>
2017-07-14 15:22:44 -0600

    Anaconda Uploading

--- devtools/anaconda/conda_upload.sh
+++ devtools/anaconda/conda_upload.sh
@@ -11,6 +11,6 @@ export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
 conda build .
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`*.tar.bz2 --force
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 ls $CONDA_BLD_PATH/$OS/

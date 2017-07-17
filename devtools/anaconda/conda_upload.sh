77a3287473321442d4c5200fdc3888e1f29988bc
nividic <gcalabro@uci.edu>
2017-07-17 09:45:53 -0600

    Anaconda Uploading

--- devtools/anaconda/conda_upload.sh
+++ devtools/anaconda/conda_upload.sh
@@ -9,6 +9,6 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
-conda build .
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l main $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`*.tar.bz2 --force
+conda build --python 3.5 .
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l main $CONDA_BLD_PATH/$OS/$PKG_NAME-*.tar.bz2 --force
 anaconda logout
